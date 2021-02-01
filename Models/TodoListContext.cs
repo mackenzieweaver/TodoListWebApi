84a888712a79033a0a2ed32a479315ba3824ec80
Mack <macknz7@gmail.com>
2021-02-01 10:50:01 -0800

    Initial commit

---
+++ Models/TodoListContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoListWebApi.Models
+{
+    public class TodoListContext : DbContext
+    {
+        public TodoListContext(DbContextOptions<TodoListContext> options) : base(options)
+        {
+        }
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# TodoListWebApi
+ https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-5.0&tabs=visual-studio
